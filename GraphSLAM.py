6443e15df4a66b6b5204fb471834e77c05cbe79b
Odin <odin@severinsen.no>
2020-12-23 14:22:14 +0100

    Comment out more profiling and removing kfs

--- GraphSLAM.py
+++ GraphSLAM.py
@@ -13,11 +13,7 @@ profile = line_profiler.LineProfiler()
 atexit.register(profile.print_stats)
 
 class GraphSLAM:
-    def __init__(self, p, q, r, alphas=np.array([0.001, 0.0001]), sensor_offset=np.zeros(2), keyframe_rotation = 2.5*np.pi/180, keyframe_distance = 0.5):
-        
-        # Thresholds for when to add new pose to graph
-        self.keyframe_rotation = keyframe_rotation
-        self.keyframe_distance = keyframe_distance
+    def __init__(self, p, q, r, alphas=np.array([0.001, 0.0001]), sensor_offset=np.zeros(2)):
 
         # Add noise models
         self.Q = gtsam.noiseModel.Diagonal.Sigmas(q)
@@ -31,9 +27,6 @@ class GraphSLAM:
         self.estimates = gtsam.Values()
         self.graph = gtsam.NonlinearFactorGraph()
 
-        # Start in origin
-        self.latest_pose = gtsam.Pose2(0.0, 0.0, 0.0)
-
         # To enumerate all poses and landmarks in the graph
         self.landmark_count = 1
         self.pose_count = 0
@@ -43,9 +36,11 @@ class GraphSLAM:
         self.landmarks_idxs = np.empty((0,), dtype=int)
 
         # Initilize graph with prior
+        # Start in origin
+        prior_pose = gtsam.Pose2(0.0, 0.0, 0.0)
         prior_noise = gtsam.noiseModel.Diagonal.Sigmas(p)
-        self.graph.add(gtsam.PriorFactorPose2(X(0), self.latest_pose, prior_noise))
-        self.estimates.insert(X(0), self.latest_pose)
+        self.graph.add(gtsam.PriorFactorPose2(X(0), prior_pose, prior_noise))
+        self.estimates.insert(X(0), prior_pose)
 
     def get_kf_pose(self, kf_idx):
         return self.estimates.atPose2(X(kf_idx))
@@ -53,42 +48,35 @@ class GraphSLAM:
     def get_lmk_point(self, lmk_idx):
         return self.estimates.atPoint2(lmk_idx)
 
-    @profilehooks.profile(sort="cumulative")
+    @property
+    def latest_pose(self):
+        return self.get_kf_pose(self.pose_count)
+
+    # @profilehooks.profile(sort="cumulative")
     def new_odometry(self, u):
         """
         Does dead-reckoning on newest state. Checks if newest pose estimate is sufficiently different from last keyframe to add it to the pose graph.
         """
         # Propagate latest state
-        odom = gtsam.Pose2(u[0], u[1], u[2])
-        self.latest_pose = self.latest_pose.compose(odom)
+        odom = utils.np2pose(u)
+        last_pose = self.latest_pose
+        last_pose = last_pose.compose(odom)
         latest_keyframe_id = self.pose_count
-
-        latest_kf_pose = self.get_kf_pose(latest_keyframe_id)
 
         assert self.estimates.exists(X(latest_keyframe_id)), "new_odometry: latest_keyframe_id doesn't exist in graph??"
 
+        # New keyframe, add to pose graph
+        self.estimates.insert(X(latest_keyframe_id + 1), last_pose)
 
-        pose_diff = latest_kf_pose.between(self.latest_pose)
-
-        if (
-            # We have moved sufficiently far away
-            np.linalg.norm(pose_diff.translation()) > self.keyframe_distance
-            or 
-            # We have rotated enough
-            np.abs(pose_diff.theta()) > self.keyframe_rotation
-        ):
-            # New keyframe, add to pose graph
-            self.estimates.insert(X(latest_keyframe_id + 1), self.latest_pose)
-
-            # Connect previous keyframe to new keyframe by "virtual odometry", i.e. the difference between them
-            self.graph.add(
-                gtsam.BetweenFactorPose2(
-                    X(latest_keyframe_id), X(latest_keyframe_id + 1), pose_diff, self.Q
-                )
+        # Connect previous keyframe to new keyframe by "virtual odometry", i.e. the difference between them
+        self.graph.add(
+            gtsam.BetweenFactorPose2(
+                X(latest_keyframe_id), X(latest_keyframe_id + 1), odom, self.Q
             )
+        )
 
-            # Update pose count
-            self.pose_count = latest_keyframe_id + 1
+        # Update pose count
+        self.pose_count += 1
 
     def transform_jacobian(self, T, z):
         r, theta = z
@@ -108,7 +96,7 @@ class GraphSLAM:
 
         return Jac
 
-    @profilehooks.profile(sort="cumulative")
+    # @profilehooks.profile(sort="cumulative")
     def new_observation(self, z):
         """
         Receives a bunch of new observations, assumed to be in the same frame of latest pose. 
@@ -133,6 +121,7 @@ class GraphSLAM:
         assert z_c_i.shape == z.shape, "GraphSLAM.new_observation: z_c_i and z must have same shape"
 
         landmarks_to_add = True
+        z_nonass = z
 
         # It doesn't make sense to make associations with no prior landmarks
         if numLmks > 0:
@@ -141,33 +130,10 @@ class GraphSLAM:
             P_margs = marginals.jointMarginalCovariance(gtsam.KeyVector(np.append(X(self.pose_count), self.landmarks_idxs)))
             P = marginals.jointMarginalCovariance(gtsam.KeyVector(np.append(X(self.pose_count), self.landmarks_idxs))).fullMatrix()
 
-            # P_test = np.zeros_like(P)
-
-            # # Insert landmark - landmark covariance
-            # for i, li in zip(range(0, 2*numLmks, 2), self.landmarks_idxs):
-            #     i_idxs = slice(3+i, 3+i+2)
-            #     for j, lj in zip(range(0, 2*numLmks, 2), self.landmarks_idxs):
-            #         j_idxs = slice(3+j, 3+j+2)
-            #         P_test[i_idxs, j_idxs] = P_margs.at(li, lj)
-
-            
-            # # Insert pose - landmark covariance
-            # for i, l in enumerate(self.landmarks_idxs):
-            #     l_idx = slice(3+i,3+i+2)
-            #     P_test[:3, l_idx] = P_margs.at(X(self.pose_count), l)
-            
-            # # Due to symmetry
-            # P_test[3:, :3] = P_test[:3, 3:].T
-
-            # # Insert pose covariance
-            # P_test[:3, :3] = P_margs.at(X(self.pose_count), X(self.pose_count))
-
             # We need to permute P to move pose from bottom-right to top-left
             P = np.roll(P, (3,3), axis=(0,1))
 
-            # assert np.allclose(P_test, P), "P_test not same as P"
-
-            latest_kf_pose = self.get_kf_pose(self.pose_count)
+            latest_kf_pose = self.latest_pose
 
             H = self.H(latest_kf_pose)
             S = H @ P @ H.T
@@ -176,27 +142,10 @@ class GraphSLAM:
             ## block diag indices for 2 x 2 blocks, broadcast R
             S[idxs[..., None], idxs[:, None]] += self.R.covariance()[None]
 
-            T_ib = self.latest_pose
-
-            # Transform from inertial to keyframe
-            T_ik = self.get_kf_pose(self.pose_count)
-
-            # Transform from body to keyframe
-            T_kb = T_ik.between(T_ib)
-
-            z_c = utils.polar2cart(z)
-
-            # Cartesian coordinate of measurements in keyframe
-            z_c_k = utils.transform(T_kb, z_c)
-
-            # Polar coordinates, ordering consistent with lmk_idx
-            z_p_k = utils.cart2polar(z_c_k).ravel()
-
-            # z = z.ravel()
-            latest_kf_pose = self.get_kf_pose(self.pose_count)
             z_pred = self.h(latest_kf_pose)
+            z = z.ravel()
 
-            a = JCBB(z_p_k, z_pred, S, self.alphas[0], self.alphas[1])
+            a = JCBB(z, z_pred, S, self.alphas[0], self.alphas[1])
 
             # Needs to reshape back
             z = z.reshape(-1,2)
@@ -207,28 +156,9 @@ class GraphSLAM:
             # Needs to tranform measurements to being relative keyframe
             zass = z[a > -1]
             
-            # Cartesian measurement in sensor frame
-            zass_s = utils.polar2cart(zass)
-            
-            # We skip the transform between body frame and sensor frame as these cancel in the end (the sensor frame is fixed relative body frame)
-            # Transform from body to inertial 
-            T_ib = self.latest_pose
-
-            # Transform from inertial to keyframe
-            T_ik = self.get_kf_pose(self.pose_count)
-
-            # Transform from body to keyframe
-            T_kb = T_ik.between(T_ib)
-
-            # Cartesian coordinate of measurements in keyframe
-            zass_k = utils.transform(T_kb, zass_s)
-
-            # Polar coordinates, ordering consistent with lmk_idx
-            zass_kp = utils.cart2polar(zass_k)
-
             # Get two lists for ranges and bearings for convenience
-            ranges = zass_kp[:,0]
-            bearings = [gtsam.Rot2(b) for b in zass_kp[:,1]]
+            ranges = zass[:,0]
+            bearings = [gtsam.Rot2(b) for b in zass[:,1]]
 
             # Index of landmarks that have been associated. Must start at 1 as 1-indexed
             associated_lmk_idxs = a[a > -1] + 1
@@ -267,19 +197,16 @@ class GraphSLAM:
             # Get latest landmark idx (we start counting on 1)
             lmk_idx = self.landmarks_idxs.shape[0]
             kf_idx = self.pose_count
-            latest_kf_pose = self.get_kf_pose(kf_idx)
+            latest_kf_pose = self.latest_pose
             
-            for l in z_c_i:
+            for l, (l_range, l_bearing) in zip(z_c_i, z_nonass):
                 # We add the inertial position of the landmark directly to the graph
                 lmk_idx += 1
                 self.estimates.insert(L(lmk_idx), l)
-                # We need to transform range-bearing to latest keyframe
-                l_b = latest_kf_pose.transformTo(l)
-                l_range = np.linalg.norm(l_b)
-                l_bearing = gtsam.Rot2.relativeBearing(l_b)
 
                 self.graph.add(gtsam.BearingRangeFactor2D(
-                    X(kf_idx), L(lmk_idx), l_bearing, l_range, self.R))
+                    X(kf_idx), L(lmk_idx), gtsam.Rot2(l_bearing), l_range, self.R)
+                )
 
                 # Add new landmark to buffer for later
                 self.landmarks_idxs = np.append(self.landmarks_idxs, L(lmk_idx))
@@ -289,7 +216,7 @@ class GraphSLAM:
 
         return a + 1
 
-    @profilehooks.profile(sort="cumulative")
+    # @profilehooks.profile(sort="cumulative")
     def h(self, pose):
         T_bi = pose.inverse() # Transform from inertial to body
         T_sb = self.sensor_offset.inverse() # Transform from body to sensor
@@ -301,7 +228,119 @@ class GraphSLAM:
 
         return z_pred
 
-    @profilehooks.profile(sort="cumulative")
+    # @profilehooks.profile(sort="cumulative")
+    def Hm(self, pose) -> np.ndarray:
+        """Calculate the jacobian of h.
+        Parameters
+        ----------
+        eta : np.ndarray, shape=(3 + 2 * #landmarks,)
+            The robot state and landmarks stacked.
+        Returns
+        -------
+        np.ndarray, shape=(2 * #landmarks, 3 + 2 * #landmarks)
+            the jacobian of h wrt. eta.
+        """
+        # extract states and map
+        x = self.latest_pose #eta[0:3]
+        ## reshape map (2, #landmarks), m[j] is the jth landmark
+        m = self.landmarks #eta[3:].reshape((-1, 2)).T
+
+        numM = m.shape[0]
+
+        R_ib = x.rotation().matrix()
+
+        delta_m = m - x.translation() # relative position of landmarks to body frame in inertial frame
+
+        # Arm from body to sensor in body frame
+        r_bs_b = self.sensor_offset.translation()
+
+        # Cartesian coordinates of landmarks relative sensor in inertial frame
+        zc = delta_m - r_bs_b@R_ib.T
+
+        # Transpose here to make dimensions correct later
+        delta_m = delta_m.T
+        zc = zc.T
+
+        zpred = self.h(pose).reshape(-1, 2).T
+        zr = zpred[0]
+
+        Rpihalf = utils.rot_mat(np.pi / 2)
+
+        # Allocate H and set submatrices as memory views into H
+        H = np.zeros((2 * numM, 3 + 2 * numM))
+        Hx = H[:, :3]  # slice view, setting elements of Hx will set H as well
+        Hm = H[:, 3:]  # slice view, setting elements of Hm will set H as well
+
+        jac_Zcb = -np.eye(2, 3)  # preallocate and update this for speed
+        for i in range(numM):  # this whole loop can be vectorized
+            ind = 2 * i
+            inds = slice(ind, ind + 2)
+            jac_Zcb[:, 2] = -Rpihalf @ delta_m[:, i]
+
+            Hx[ind] = (zc[:, i] / zr[i]) @ jac_Zcb
+            Hx[ind + 1] = (zc[:, i] / zr[i] ** 2) @ Rpihalf.T @ jac_Zcb
+
+            Hm[inds, inds] = -Hx[inds, :2]
+
+        return Hm
+
+    # @profilehooks.profile(sort="cumulative")
+    def Hx(self, pose) -> np.ndarray:
+        """Calculate the jacobian of h.
+        Parameters
+        ----------
+        eta : np.ndarray, shape=(3 + 2 * #landmarks,)
+            The robot state and landmarks stacked.
+        Returns
+        -------
+        np.ndarray, shape=(2 * #landmarks, 3 + 2 * #landmarks)
+            the jacobian of h wrt. eta.
+        """
+        # extract states and map
+        x = self.latest_pose #eta[0:3]
+        ## reshape map (2, #landmarks), m[j] is the jth landmark
+        m = self.landmarks #eta[3:].reshape((-1, 2)).T
+
+        numM = m.shape[0]
+
+        R_ib = x.rotation().matrix()
+
+        delta_m = m - x.translation() # relative position of landmarks to body frame in inertial frame
+
+        # Arm from body to sensor in body frame
+        r_bs_b = self.sensor_offset.translation()
+
+        # Cartesian coordinates of landmarks relative sensor in inertial frame
+        zc = delta_m - r_bs_b@R_ib.T
+
+        # Transpose here to make dimensions correct later
+        delta_m = delta_m.T
+        zc = zc.T
+
+        zpred = self.h(pose).reshape(-1, 2).T
+        zr = zpred[0]
+
+        Rpihalf = utils.rot_mat(np.pi / 2)
+
+        # Allocate H and set submatrices as memory views into H
+        H = np.zeros((2 * numM, 3 + 2 * numM))
+        Hx = H[:, :3]  # slice view, setting elements of Hx will set H as well
+        Hm = H[:, 3:]  # slice view, setting elements of Hm will set H as well
+
+        jac_Zcb = -np.eye(2, 3)  # preallocate and update this for speed
+        for i in range(numM):  # this whole loop can be vectorized
+            ind = 2 * i
+            inds = slice(ind, ind + 2)
+            jac_Zcb[:, 2] = -Rpihalf @ delta_m[:, i]
+
+            Hx[ind] = (zc[:, i] / zr[i]) @ jac_Zcb
+            Hx[ind + 1] = (zc[:, i] / zr[i] ** 2) @ Rpihalf.T @ jac_Zcb
+
+            Hm[inds, inds] = -Hx[inds, :2]
+
+        return Hx
+
+    # @profilehooks.profile(sort="cumulative")
     def H(self, pose) -> np.ndarray:
         """Calculate the jacobian of h.
         Parameters
@@ -358,7 +397,7 @@ class GraphSLAM:
         return H
 
     # Full optimizer of pose graph
-    @profilehooks.profile(sort="cumulative")
+    # @profilehooks.profile(sort="cumulative")
     def optimize(self):
         params = gtsam.LevenbergMarquardtParams()
         optimizer = gtsam.LevenbergMarquardtOptimizer(self.graph, self.estimates, params)
